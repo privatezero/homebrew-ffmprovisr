31c882f95a93fe79265350c358c81269d373010d
Andrew Weaver <weevz@uw.edu>
2017-05-04 13:09:26 -0400

    Create ffmprovisr.rb

---
+++ ffmprovisr.rb
@@ -0,0 +1,12 @@
+class Uwmediatools < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/amiaopensource/ffmprovisr/archive/v2017-04-19.tar.gz"
+  
+  sha256 "4120fb650d35ab8fa8b96e6ec78740b3ade27249dd4193d858ffb1956a6098dc"
+
+  def install
+  prefix.install Dir["output/*"]
+  end
+ 
+end
